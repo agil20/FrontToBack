f014e540b3807c9fd3c1e4a6d52ba2558cb55806
agil20 <100156146+agil20@users.noreply.github.com>
2022-06-22 06:30:12 -0700

    first commit

---
+++ FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+          
+            HomeVM homeVm=new HomeVM();
+            homeVm.Sliders = _context.Sliders.ToList();
+            homeVm.SliderContent = _context.SliderContents.FirstOrDefault();
+            return View(homeVm);
+        }
+    }
+}
---
+++ FrontToBack/FrontToBack/ViewModels/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using FrontToBack.Models;
+using System.Collections.Generic;
+
+namespace FrontToBack.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider>Sliders { get; set; }
+        public SliderContent SliderContent { get; set; }
+
+
+    }
+}
