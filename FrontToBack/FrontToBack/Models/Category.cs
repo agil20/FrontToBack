89b7c19cfe70bbd973049dd4227111f2f0bf6baa
agil20 <100156146+agil20@users.noreply.github.com>
2022-06-22 06:39:29 -0700

    feat:Modelse product ve catagory elave eledim

---
+++ FrontToBack/FrontToBack/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace FrontToBack.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ FrontToBack/FrontToBack/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace FrontToBack.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int ImageUrl { get; set; }
+    }
+}
