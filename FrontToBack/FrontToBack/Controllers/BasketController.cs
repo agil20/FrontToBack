f530e9812cba1a9fce042e275f3f951455493568
agil20 <100156146+agil20@users.noreply.github.com>
2022-07-01 07:38:48 -0700

    feat:"baskete atilmaq mentini yazdim"

--- FrontToBack/FrontToBack/Controllers/BasketController.cs
+++ FrontToBack/FrontToBack/Controllers/BasketController.cs
@@ -1,23 +1,76 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FrontToBack.Controllers
 {
     public class BasketController : Controller
     {
+
+        private readonly AppDbContext _context;
+
+        public BasketController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult AddItem()
+        public async Task<IActionResult> AddItem(int? id)
         {
-            HttpContext.Session.SetString("Person", "Aqil");
-            return Content("");
+            //HttpContext.Session.SetString("Person", "Aqil");
+            //Response.Cookies.Append("group", "p322", new CookieOptions {MaxAge=TimeSpan.FromDays(14) });
+            if (id == null) return NotFound();
+
+            Product dbProduct = await _context.Products.FindAsync(id);
+
+            if (dbProduct == null) return NotFound();
+            List<BasketVM> products;
+            if (Request.Cookies["basket"] == null)
+            {
+                products = new List<BasketVM>();
+            }
+            else {
+                products = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            }
+            BasketVM existProduct = products.Find(x => x.Id == id);
+            if (existProduct == null)
+            {
+                BasketVM basketVM = new BasketVM
+                {
+                    Id = dbProduct.Id,
+                    Name = dbProduct.Name,
+                    Price = dbProduct.Price,
+                    ImageUrl = dbProduct.ImageUrl,
+                    CategoryId = dbProduct.CategoryId,
+                    ProductCount = 1
+                };
+                products.Add(basketVM);
+            }
+            else 
+            {
+                existProduct.ProductCount++;
+            
+            
+            }
+     
+         
+            Response.Cookies.Append("basket",JsonConvert.SerializeObject(products),new CookieOptions { MaxAge=TimeSpan.FromDays(5)});
+            return RedirectToAction("index","home");
         }
         public IActionResult ShowItem()
         {
-         string name=   HttpContext.Session.GetString("Person");
-            return Content(name);
+            string basket = Request.Cookies["basket"];
+              List< BasketVM> p=JsonConvert.DeserializeObject<List<BasketVM>>(basket);
+            return View(p);
         }
     }
 }
--- FrontToBack/FrontToBack/ViewModels/BasketVM.cs
+++ FrontToBack/FrontToBack/ViewModels/BasketVM.cs
@@ -1,14 +1,12 @@
-﻿namespace FrontToBack.Models
+﻿namespace FrontToBack.ViewModels
 {
-    public class Product
+    public class BasketVM
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
         public string ImageUrl { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
-        public int Count { get; set; }
-
+        public int ProductCount { get; set; }
     }
 }
