59bb71b6a90614a1ac5728f6f903cb2e4632c4b5
agil20 <100156146+agil20@users.noreply.github.com>
2022-07-01 13:37:00 -0700

    feat:"Basketde total ,couunt u hell etdim"

--- FrontToBack/FrontToBack/ViewComponents/HeaderViewComponent.cs
+++ FrontToBack/FrontToBack/ViewComponents/HeaderViewComponent.cs
@@ -1,6 +1,9 @@
 ﻿using FrontToBack.DAL;
 using FrontToBack.Models;
+using FrontToBack.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +19,21 @@ namespace FrontToBack.ViewComponents
         }
         public async Task<IViewComponentResult> InvokeAsync() 
         {
+            double total = 0;
+            ViewBag.Total = 0;
+            ViewBag.BasketCount = 0;
+            string basket = Request.Cookies["basket"];
+            if (basket != null)
+            {
+                List<BasketVM> products = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+                ViewBag.BasketCount = products.Count;
+                foreach (var item in products)
+                {
+                    total = item.Price * item.ProductCount;
+                }
+                ViewBag.BasketCount=total;
+
+            }
             Bio bio = _context.Bios.FirstOrDefault();
             return View(await Task.FromResult(bio));
         
