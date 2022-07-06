2e8270bb7bf1e6e2514a1cd778bbb4291f0e20c1
agil20 <100156146+agil20@users.noreply.github.com>
2022-07-06 06:13:00 -0700

    feat:     ClassicEditor elave eledim

--- FrontToBack/FrontToBack/Models/Product.cs
+++ FrontToBack/FrontToBack/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FrontToBack.Models
 {
@@ -8,6 +9,8 @@ namespace FrontToBack.Models
         public string Name { get; set; }
         public int Price { get; set; }
         public string ImageUrl { get; set; }
+        //[NotMapped]
+        //public IFromFile MyProperty { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public int Count { get; set; }
