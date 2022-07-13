13d13fa424660b3f00ee31332bed1944df539d07
agil20 <100156146+agil20@users.noreply.github.com>
2022-07-13 04:19:58 -0700

    feat:Login helle dtim

--- FrontToBack/FrontToBack/ViewModels/LoginVM.cs
+++ FrontToBack/FrontToBack/ViewModels/LoginVM.cs
@@ -2,13 +2,14 @@
 
 namespace FrontToBack.ViewModels
 {
-    public class LoginVM
-    {
-        [Required]
-        public string UserName { get; set; }
-        [Required, DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
-        [Required,DataType(DataType.Password)]
-        public string Password { get; set; }
+    
+        public class LoginVM
+        {
+            [Required, DataType(DataType.EmailAddress)]
+            public string Email { get; set; }
+
+            [Required, DataType(DataType.Password)]
+            public string Password { get; set; }
+        
     }
 }
