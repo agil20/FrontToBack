dd63fa19dc29a6cd5089c83c66f7172835118508
agil20 <100156146+agil20@users.noreply.github.com>
2022-07-13 03:04:22 -0700

    feat: register hell etdim

--- FrontToBack/FrontToBack/Controllers/AccountController.cs
+++ FrontToBack/FrontToBack/Controllers/AccountController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace FrontToBack.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _rolemanager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,RoleManager<IdentityRole>rolemanager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _rolemanager = rolemanager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +27,31 @@ namespace FrontToBack.Controllers
         {
 
             return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            AppUser user = new AppUser {
+            FullName=register.FullName,
+            UserName=register.UserName,
+             Email=register.Email
+            };
+     IdentityResult result=    await _userManager.CreateAsync(user, register.Password);
+            if (!result.Succeeded) 
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("",item.Description);
+                }
+                return View(register);
+            
+            }
+            return RedirectToAction("index", "Home");
 
         }
 
