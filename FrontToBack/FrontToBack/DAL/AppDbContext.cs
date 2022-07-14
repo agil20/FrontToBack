e2f7e402f1e88aeea2ac0565e7eb94f9976c4e69
agil20 <100156146+agil20@users.noreply.github.com>
2022-07-14 06:28:42 -0700

    feat: "Seed data elave eldim

--- FrontToBack/FrontToBack/DAL/AppDbContext.cs
+++ FrontToBack/FrontToBack/DAL/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using FrontToBack.Models;
+﻿using FrontToBack.Migrations;
+using FrontToBack.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,44 @@ namespace FrontToBack.DAL
         public DbSet<Category> Categories{ get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Bio> Bios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Slider>().HasData(
+new Slider { Id=1,ImgUrl= "h3-slider-background.jpg" },
+new Slider { Id = 2, ImgUrl = "h3-slider-background-2.jpg" }
+
+                );
+            builder.Entity<Product>().HasData(
+new Product { Id = 1, Name="Product1",Price=60,ImageUrl= "shop-13-img.jpg" ,CategoryId=1},
+new Product { Id = 2, Name = "Product2", Price = 65, ImageUrl = "shop-12-img.jpg", CategoryId = 2 },
+new Product { Id = 3, Name = "Product3", Price = 70, ImageUrl = "shop-11-img.jpg", CategoryId = 3 },
+new Product { Id = 4, Name = "Product4", Price = 80, ImageUrl = "shop-13-img.jpg", CategoryId = 4 },
+new Product { Id = 5, Name = "Product5", Price = 90, ImageUrl = "shop-8-img.jpg", CategoryId = 5 },
+new Product { Id = 6, Name = "Product6", Price = 90, ImageUrl = "shop-11-img.jpg", CategoryId = 3 }
+
+
+               );
+            builder.Entity<Category>().HasData(
+new Category { Id = 1, Name="Popular",Desc="Lorem ipsum" },
+new Category { Id = 2, Name = "Winter", Desc = "Lorem ipsum" },
+new Category { Id = 3, Name = "Exotic", Desc = "Lorem ipsum" },
+new Category { Id = 4, Name = "Varius", Desc = "Lorem ipsum" },
+new Category { Id = 5, Name = "Venor", Desc = "Lorem ipsum" }
+);
+            builder.Entity<Bio>().HasData(
+new Bio { Id = 1,Linkedn= "https://www.linkexdin.com/" ,Facebook= "Cj0KCQjwtvqVBhCVARIsAFUxcRv7PtwND0mb_zQZOHEyCwzR8nce3lB8LQyvcAf8uu_Cb6agQSJCTJgaAkJZEALw_wcB",AuthorName="Aqil" }
+
+
+             );
+            builder.Entity<Bio>().HasData(
+new Bio { Id = 1, Linkedn = "https://www.linkexdin.com/", Facebook = "Cj0KCQjwtvqVBhCVARIsAFUxcRv7PtwND0mb_zQZOHEyCwzR8nce3lB8LQyvcAf8uu_Cb6agQSJCTJgaAkJZEALw_wcB", AuthorName = "Aqil" }
+
+
+     );
+
+        }
 
     }
 }
