0550989848a452250bac07049f832054050c1351
agil20 <100156146+agil20@users.noreply.github.com>
2022-07-15 05:52:29 -0700

    feat:user controller,role controller yaratdim

---
+++ FrontToBack/FrontToBack/Areas/AdminPanel/Controllers/RoleController.cs
@@ -0,0 +1,32 @@
+﻿using FrontToBack.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FrontToBack.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class RoleController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _rolemanager;
+
+        public RoleController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> rolemanager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _rolemanager = rolemanager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_rolemanager.Roles.ToList());
+        }
+        public IActionResult Create()
+        { 
+        
+            return View();
+        }
+    }
+}
---
+++ FrontToBack/FrontToBack/Areas/AdminPanel/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using FrontToBack.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FrontToBack.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _rolemanager;
+
+        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> rolemanager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _rolemanager = rolemanager;
+        }
+
+        public IActionResult Index(string search)
+        {
+            var users=search==null?
+                _userManager.Users.ToList():_userManager.Users.
+                Where(user=>user.FullName.ToLower().Contains(search.ToLower())).ToList();
+  
+
+            return View(users);
+        }
+    }
+}
